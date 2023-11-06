0ce92814006c3e6dbbf431b92ae6a1c4adad1d6f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-06 20:43:29 +0000

    Updating to latest FlutterFlow output.

--- lib/homepage/homepage_widget.dart
+++ lib/homepage/homepage_widget.dart
@@ -1,10 +1,12 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'homepage_model.dart';
 export 'homepage_model.dart';
 
@@ -21,6 +23,22 @@ class _HomepageWidgetState extends State<HomepageWidget>
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
+  final animationsMap = {
+    'containerOnActionTriggerAnimation': AnimationInfo(
+      trigger: AnimationTrigger.onActionTrigger,
+      applyInitialState: true,
+      effects: [
+        MoveEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 300.ms,
+          begin: const Offset(-40.0, 0.0),
+          end: const Offset(0.0, 0.0),
+        ),
+      ],
+    ),
+  };
+
   @override
   void initState() {
     super.initState();
@@ -34,6 +52,12 @@ class _HomepageWidgetState extends State<HomepageWidget>
       length: 3,
       initialIndex: 0,
     )..addListener(() => setState(() {}));
+    setupAnimations(
+      animationsMap.values.where((anim) =>
+          anim.trigger == AnimationTrigger.onActionTrigger ||
+          !anim.applyInitialState),
+      this,
+    );
   }
 
   @override
@@ -109,6 +133,106 @@ class _HomepageWidgetState extends State<HomepageWidget>
                           fontSize: 18.0,
                           fontWeight: FontWeight.w500,
                         ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 12.0, 0.0),
+                    child: InkWell(
+                      splashColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      onTap: () async {
+                        if ((Theme.of(context).brightness ==
+                                Brightness.light) ==
+                            true) {
+                          setDarkModeSetting(context, ThemeMode.dark);
+                          if (animationsMap[
+                                  'containerOnActionTriggerAnimation'] !=
+                              null) {
+                            animationsMap['containerOnActionTriggerAnimation']!
+                                .controller
+                                .forward(from: 0.0);
+                          }
+                        } else {
+                          setDarkModeSetting(context, ThemeMode.light);
+                          if (animationsMap[
+                                  'containerOnActionTriggerAnimation'] !=
+                              null) {
+                            animationsMap['containerOnActionTriggerAnimation']!
+                                .controller
+                                .reverse();
+                          }
+                        }
+                      },
+                      child: Container(
+                        width: 80.0,
+                        height: 40.0,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF1F4F8),
+                          borderRadius: BorderRadius.circular(20.0),
+                          border: Border.all(
+                            color: const Color(0xFFE0E3E7),
+                            width: 1.0,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.fromSTEB(
+                              2.0, 2.0, 2.0, 2.0),
+                          child: Stack(
+                            alignment: const AlignmentDirectional(0.0, 0.0),
+                            children: [
+                              const Align(
+                                alignment: AlignmentDirectional(-0.90, 0.00),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      6.0, 0.0, 0.0, 0.0),
+                                  child: Icon(
+                                    Icons.wb_sunny_rounded,
+                                    color: Color(0xFF57636C),
+                                    size: 24.0,
+                                  ),
+                                ),
+                              ),
+                              const Align(
+                                alignment: AlignmentDirectional(1.00, 0.00),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0.0, 0.0, 6.0, 0.0),
+                                  child: Icon(
+                                    Icons.mode_night_rounded,
+                                    color: Color(0xFF57636C),
+                                    size: 24.0,
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: const AlignmentDirectional(1.00, 0.00),
+                                child: Container(
+                                  width: 36.0,
+                                  height: 36.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        blurRadius: 4.0,
+                                        color: Color(0x430B0D0F),
+                                        offset: Offset(0.0, 2.0),
+                                      )
+                                    ],
+                                    borderRadius: BorderRadius.circular(30.0),
+                                    shape: BoxShape.rectangle,
+                                  ),
+                                ).animateOnActionTrigger(
+                                  animationsMap[
+                                      'containerOnActionTriggerAnimation']!,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                   const Row(
                     mainAxisSize: MainAxisSize.max,
