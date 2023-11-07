d7a54c1dbe906e5d3b080aa5c57da115ef07c9a1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-07 21:27:20 +0000

    Updating to latest FlutterFlow output.

--- lib/detalhes_produtos/detalhes_produtos_widget.dart
+++ lib/detalhes_produtos/detalhes_produtos_widget.dart
@@ -1,4 +1,5 @@
 import '/backend/schema/structs/index.dart';
+import '/components/preferencias_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
@@ -428,8 +429,28 @@ class _DetalhesProdutosWidgetState extends State<DetalhesProdutosWidget> {
                               setState(() {
                                 FFAppState().quantity = 1;
                               });
-
-                              context.pushNamed('carrinho');
+                              await showModalBottomSheet(
+                                isScrollControlled: true,
+                                backgroundColor: Colors.transparent,
+                                enableDrag: false,
+                                context: context,
+                                builder: (context) {
+                                  return GestureDetector(
+                                    onTap: () => _model
+                                            .unfocusNode.canRequestFocus
+                                        ? FocusScope.of(context)
+                                            .requestFocus(_model.unfocusNode)
+                                        : FocusScope.of(context).unfocus(),
+                                    child: Padding(
+                                      padding: MediaQuery.viewInsetsOf(context),
+                                      child: const SizedBox(
+                                        height: 750.0,
+                                        child: PreferenciasWidget(),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ).then((value) => safeSetState(() {}));
                             },
                             child: Row(
                               mainAxisSize: MainAxisSize.max,
