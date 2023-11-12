0a726297a82a1f2671e2b706825d9af3c35d1004
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-12 19:35:49 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/carrinho/carrinho_widget.dart
+++ lib/pages/carrinho/carrinho_widget.dart
@@ -6,6 +6,7 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/random_data_util.dart' as random_data;
 import 'package:aligned_dialog/aligned_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -832,16 +833,10 @@ class _CarrinhoWidgetState extends State<CarrinhoWidget>
                                                                 FFAppState()
                                                                     .contador]
                                                             .massaNome,
-                                                        numberProdutos: (buttonListaCarrinhoPedidosRecordList[buttonListaCarrinhoPedidosRecordList
-                                                                    .where((e) =>
-                                                                        e !=
-                                                                        null)
-                                                                    .toList()[
-                                                                        FFAppState()
-                                                                            .contador]
-                                                                    .numberProdutos]
-                                                                .numberProdutos) +
-                                                            1,
+                                                        numberProdutos:
+                                                            random_data
+                                                                .randomInteger(
+                                                                    1, 1000),
                                                       ));
                                                   await Future.delayed(
                                                       const Duration(
